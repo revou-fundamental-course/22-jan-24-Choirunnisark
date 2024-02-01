3c4b7699000e7685e33915709efd6b586002bd7f
Choirunnisark <choirunnisa.rz14@gmail.com>
2024-02-01 22:49:19 +0700

    buat halaman html css javascript

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Selecting form and input elements
+const form = document.querySelector("form");
+const passwordInput = document.getElementById("password");
+
+// Function to display error messages
+const showError = (field, errorText) => {
+    field.classList.add("error");
+    const errorElement = document.createElement("small");
+    errorElement.classList.add("error-text");
+    errorElement.innerText = errorText;
+    field.closest(".form-group").appendChild(errorElement);
+}
+
+// Function to handle form submission
+const handleFormData = (e) => {
+    e.preventDefault();
+
+    // Retrieving input elements
+    const fullnameInput = document.getElementById("fullname");
+    const emailInput = document.getElementById("email");
+    const genderInput = document.getElementById("interested");
+
+    // Getting trimmed values from input fields
+    const fullname = fullnameInput.value.trim();
+    const email = emailInput.value.trim();
+    const interested = genderInput.value;
+
+    // Regular expression pattern for email validation
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    // Clearing previous error messages
+    document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
+    document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
+
+    // Performing validation checks
+    if (fullname === "") {
+        showError(fullnameInput, "Enter your full name");
+    }
+    if (!emailPattern.test(email)) {
+        showError(emailInput, "Enter a valid email address");
+    }
+    if (interested === "") {
+        showError(genderInput, "Select your interested");
+    }
+
+    // Checking for any remaining errors before form submission
+    const errorInputs = document.querySelectorAll(".form-group .error");
+    if (errorInputs.length > 0) return;
+
+    // Submitting the form
+    form.submit();
+}
+// Handling form submission event
+form.addEventListener("submit", handleFormData);
